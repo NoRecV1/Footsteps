65b54417a40746f3b4d5df431510eb76fccc49b5
Remi COMBE <remi.combe@lgce.fr>
2021-11-17 08:08:06 +0100

    WIP: list strange requests (from different initiator than host)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit {
   title = 'footsteps';
 
   public tabId!: number;
+  public tabHostname?: string;
 
   public tabs_latest_request_array$: ReplaySubject<{ [key: number]: chrome.webRequest.WebRequestBodyDetails[] }> = new ReplaySubject();
   public latestRequests$ = this.tabs_latest_request_array$.pipe(
@@ -25,10 +26,11 @@ export class AppComponent implements OnInit {
   public domainRequestCount$ = this.latestRequests$.pipe(
     //map list of request to object associating domain (key) to number of requests (value)
     map((requests) => requests.reduce((acc: {[key: string] : number}, request) => {
-      // const initiator = request.initiator ?? '__unknown__';
-      const domain = new URL(request.url).hostname;
-      // if (initiator === '__unknown__') console.log('Unknown initiator : ', request);
-      return (acc[domain] = ++acc[domain] || 1, acc);
+      // const initiatorHost = hostnameFromStringURL(request.initiator);
+      // if (initiatorHost && initiatorHost !== this.tabHostname) //TODO: list domain firing requets when they are not the current tab domain
+      const destDomain = hostnameFromStringURL(request.url) ?? '__error_invalid_url__';
+      if (destDomain === this.tabHostname) return acc; // ignore request if to domain of the tab
+      return (acc[destDomain] = ++acc[destDomain] || 1, acc);
     }, {})),
   );
 
@@ -37,8 +39,12 @@ export class AppComponent implements OnInit {
   ) {}
 
   public async ngOnInit () {
-    this.tabId = (await getTab()).id ?? -1;
+    const tab = await getTab();
+
+    this.tabId = tab.id ?? -1;
+    this.tabHostname = hostnameFromStringURL(tab.url ?? tab.pendingUrl);
     this.tabs_latest_request_array$.next({});
+
     chrome.storage.onChanged.addListener((changes, area) => {
       //get changes on local storages and use if tabs_latest_request_array was updated
       if (area === 'local' && changes.tabs_latest_request_array?.newValue) {
@@ -46,6 +52,12 @@ export class AppComponent implements OnInit {
           this.tabs_latest_request_array$.next(changes.tabs_latest_request_array.newValue);
         })
       }
+    });
+    chrome.tabs.onUpdated.addListener((tabId, infos, tab) => {
+      if (tabId !== this.tabId) return;
+      this.ngZone.run(() => {
+        this.tabHostname = hostnameFromStringURL(tab.url ?? tab.pendingUrl);
+      });
     });
   }
 }
@@ -63,4 +75,12 @@ function getTab (): Promise<chrome.tabs.Tab> {
       reject(e);
     }
   })
+}
+
+function hostnameFromStringURL (stringUrl: any): string | undefined {
+  let hostname;
+  try {
+    hostname = new URL(stringUrl).hostname;
+  } catch (_) {}
+  return hostname;
 }
