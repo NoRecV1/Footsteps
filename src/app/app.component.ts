5c06fc760e84a2a7f03bf6556a5ef3f80c44ea6e
NoRecV1 <rec0n.livegaming@gmail.com>
2021-11-19 15:17:59 +0100

    Interface 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import {Component } from '@angular/core';
+import {AfterViewInit, Component } from '@angular/core';
+declare var ui:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements AfterViewInit{
 
   constructor() {}
+
+  ngAfterViewInit(): void {
+    ui();
+  }
 
   title = 'footsteps';
   stalkers = ['facebook.com', 'instagram.com']
