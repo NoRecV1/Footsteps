cc568cf2a0ab84db6459225544f76c6ca6aa7838
Remi COMBE <remi.combe@lgce.fr>
2021-11-28 11:49:58 +0100

    background: remove useless values and improve varaibles setup (#4)

--- src/background.ts
+++ src/background.ts
@@ -3,33 +3,32 @@ chrome.storage.local.set({key: keepAliveTime});
 // time in latest arrays in seconds
 
 let request_array: chrome.webRequest.WebRequestBodyDetails[] = [];
-let latest_request_array: chrome.webRequest.WebRequestBodyDetails[] = [];
 let tabs_latest_request_array: { [key: number]: chrome.webRequest.WebRequestBodyDetails[]} = {};
 
-
-const setupVariables = () => chrome.storage.local.set({
-  request_array: [],
-  latest_request_array: [],
-  tabs_latest_request_array: {},
+const setupVariables = () => chrome.storage.local.get(['request_array', 'tabs_latest_request_array'], ({request_array, tabs_latest_request_array}) => {
+  chrome.storage.local.set({
+    request_array: request_array ?? [],
+    tabs_latest_request_array: tabs_latest_request_array ?? {},
+  });
 });
 
 
 // set up default values
 chrome.windows.onCreated.addListener(setupVariables);
 chrome.runtime.onInstalled.addListener(setupVariables);
-chrome.tabs.onCreated.addListener((tab) => tabs_latest_request_array[tab.id ?? -1] = []);
+chrome.tabs.onCreated.addListener((tab) => {
+  if (tab.id) tabs_latest_request_array[tab.id] = [];
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
   (details: chrome.webRequest.WebRequestBodyDetails) => {
     console.log(`[at ${Math.round(details?.timeStamp)}] Request sent in tab n°${details?.tabId} from ${details?.initiator} to ${(details?.url.length > 60) ? details?.url.substr(0, 60 - 1) + '…' : details?.url}\n\tBody details :`, details);
 
     request_array = (request_array ?? []).concat(details);
-    latest_request_array = (latest_request_array ?? []).concat(details);
     tabs_latest_request_array[details.tabId] = (tabs_latest_request_array[details.tabId] ?? []).concat(details);
 
     chrome.storage.local.set({
       request_array,
-      latest_request_array,
       tabs_latest_request_array,
     });
 
@@ -39,10 +38,8 @@ chrome.webRequest.onBeforeRequest.addListener(
     });; 
     console.log(keepAliveTime);
     setTimeout(() => {
-      latest_request_array = (latest_request_array ?? []).slice(1);
       tabs_latest_request_array[details.tabId] = (tabs_latest_request_array[details.tabId] ?? []).slice(1);
       chrome.storage.local.set({
-        latest_request_array,
         tabs_latest_request_array,
       });
     }, keepAliveTime);
